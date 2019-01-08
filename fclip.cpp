16b6074b7769a132051a29ab1d108ed4258e64d0
urbans0ft <nico@urbansoft.eu>
2019-01-08 16:38:23 +0100

    Create fclip.cpp

---
+++ fclip.cpp
@@ -0,0 +1,37 @@
+#include <Windows.h>
+#include <Shlobj.h> // DROPFILES
+#include <tchar.h>  // e.g. _tcslen
+
+int _tmain(int argc, TCHAR* argv[])
+{
+    // calculate *bytes* needed for memory allocation
+    int clpSize = sizeof(DROPFILES);
+    for (int i = 1; i < argc; i++)
+        clpSize += sizeof(TCHAR) * (_tcslen(argv[i]) + 1); // + 1 => '\0'
+    clpSize += sizeof(TCHAR); // two \0 needed at the end
+
+    // allocate the zero initialized memory
+    HDROP hdrop   = (HDROP)GlobalAlloc(GHND, clpSize);
+    DROPFILES* df = (DROPFILES*)GlobalLock(hdrop);
+    df->pFiles    = sizeof(DROPFILES); // string offset
+#ifdef _UNICODE
+    df->fWide     = TRUE; // unicode file names
+#endif // _UNICODE
+
+    // copy the command line args to the allocated memory
+    TCHAR* dstStart = (TCHAR*)&df[1];
+    for (int i = 1; i < argc; i++)
+    {
+        _tcscpy(dstStart, argv[i]);
+        dstStart = &dstStart[_tcslen(argv[i]) + 1]; // + 1 => get beyond '\0'
+    }
+    GlobalUnlock(hdrop);
+
+    // prepare the clipboard
+    OpenClipboard(NULL);
+    EmptyClipboard();
+    SetClipboardData(CF_HDROP, hdrop);
+    CloseClipboard();
+
+    return 0;
+}
