079505f56b2435f6d654486af33fdd3b22314cb8
nurbanczyk <nico.urbanczyk@uni-bielefeld.de>
2019-01-14 11:51:44 +0100

    Pre-compiled header.
    Default setting when creating a new visual studio c++ console project.

--- fclip.cpp
+++ fclip.cpp
@@ -1,8 +1,4 @@
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#include <Shlobj.h>   // DROPFILES
-#include <ShellApi.h> // HDROP
-#include <tchar.h>    // e.g. _tcslen
+#include "pch.h" // Pre-compiled header
 
 #ifdef __CYGWIN__
 #if defined(_UNICODE) || defined(UNICODE)
---
+++ pch.h
@@ -0,0 +1,11 @@
+#ifndef PCH_H
+#define PCH_H
+
+// TODO: add headers that you want to pre-compile here
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <Shlobj.h>   // DROPFILES
+#include <ShellApi.h> // HDROP
+#include <tchar.h>    // e.g. _tcslen
+
+#endif //PCH_H
