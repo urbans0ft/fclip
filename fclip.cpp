14b3e2f4d946f3fb1d94effaa3f31d4bb2106c46
Nico <nico@urbansoft.eu>
2019-02-13 22:12:21 +0100

    Beginning separation of copy / paste.

--- fclip.cpp
+++ fclip.cpp
@@ -1,58 +1,68 @@
 #include "pch.h" // Pre-compiled header
 
+int copy(int argc, TCHAR* argv[]);
 void paste();
 void pasteByHdrop();
 void pasteByFileContents(CLIPFORMAT clFileDescriptor, CLIPFORMAT clFileContents);
 void olePaste();
 void olePaste2();
 
+// usage: fclip [-v|[file1 [file2 [...]]]]
 int run(int argc, TCHAR* argv[])
 {
-    // create / fill full file path buffer and calculate global buffer
-    TCHAR** files    = new TCHAR*[argc-1];
-    DWORD*  bufSizes = new DWORD[argc-1];
-    int     clpSize  = sizeof(DROPFILES);
-    
-    for (int i = 1; i < argc; i++) {
-        bufSizes[i-1] = GetFullPathName(argv[i], 0, NULL, NULL);
-        files[i-1]    = new TCHAR[bufSizes[i-1]];
-        GetFullPathName(argv[i], bufSizes[i-1], files[i-1], NULL);
-        if (GetFileAttributes(files[i-1]) == INVALID_FILE_ATTRIBUTES)
-            return INVALID_FILE_ATTRIBUTES;
-        // calculate *bytes* needed for memory allocation
-        clpSize += sizeof(TCHAR) * (bufSizes[i-1]);
-    }
-    clpSize += sizeof(TCHAR); // two \0 needed at the end
-    
-    // allocate the zero initialized memory
-    HDROP hdrop   = (HDROP)GlobalAlloc(GHND, clpSize);
-    DROPFILES* df = (DROPFILES*)GlobalLock(hdrop);
-    df->pFiles    = sizeof(DROPFILES); // string offset
-#ifdef _UNICODE
-    df->fWide     = TRUE; // unicode file names
-#endif // _UNICODE
+	// parse command line parameter
+	if (argc == 2 && !_tcscmp(argv[1], _T("-v")))
+		paste();
+	else
+		copy(argc, argv);
 
-    // copy the command line args to the allocated memory
-    TCHAR* dstStart = (TCHAR*)&df[1];
-    for (int i = 0; i < argc - 1; i++)
-    {
-        _tcscpy(dstStart, files[i]);
-        dstStart = &dstStart[bufSizes[i]];
-    }
-    GlobalUnlock(hdrop);
-
-    // prepare the clipboard
-    //OpenClipboard(NULL);
-    //EmptyClipboard();
-    //SetClipboardData(CF_HDROP, hdrop);
-    //CloseClipboard();
-
-	olePaste();
-	paste();
 	std::string wait;
 	std::getline(std::cin, wait);
 
     return 0;
+}
+
+int copy(int argc, TCHAR* argv[])
+{
+	// create / fill full file path buffer and calculate global buffer
+	TCHAR** files = new TCHAR*[argc - 1];
+	DWORD*  bufSizes = new DWORD[argc - 1];
+	int     clpSize = sizeof(DROPFILES);
+
+	for (int i = 1; i < argc; i++) {
+		bufSizes[i - 1] = GetFullPathName(argv[i], 0, NULL, NULL);
+		files[i - 1] = new TCHAR[bufSizes[i - 1]];
+		GetFullPathName(argv[i], bufSizes[i - 1], files[i - 1], NULL);
+		if (GetFileAttributes(files[i - 1]) == INVALID_FILE_ATTRIBUTES)
+			return INVALID_FILE_ATTRIBUTES;
+		// calculate *bytes* needed for memory allocation
+		clpSize += sizeof(TCHAR) * (bufSizes[i - 1]);
+	}
+	clpSize += sizeof(TCHAR); // two \0 needed at the end
+
+							  // allocate the zero initialized memory
+	HDROP hdrop = (HDROP)GlobalAlloc(GHND, clpSize);
+	DROPFILES* df = (DROPFILES*)GlobalLock(hdrop);
+	df->pFiles = sizeof(DROPFILES); // string offset
+#ifdef _UNICODE
+	df->fWide = TRUE; // unicode file names
+#endif // _UNICODE
+
+					  // copy the command line args to the allocated memory
+	TCHAR* dstStart = (TCHAR*)&df[1];
+	for (int i = 0; i < argc - 1; i++)
+	{
+		_tcscpy(dstStart, files[i]);
+		dstStart = &dstStart[bufSizes[i]];
+	}
+	GlobalUnlock(hdrop);
+
+	// prepare the clipboard
+	OpenClipboard(NULL);
+	EmptyClipboard();
+	SetClipboardData(CF_HDROP, hdrop);
+	CloseClipboard();
+	return 0;
 }
 
 void paste()
