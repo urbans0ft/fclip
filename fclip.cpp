f2599a8e5a4184be0550744d26b2895754d76c85
nurbanczyk <nico.urbanczyk@uni-bielefeld.de>
2019-09-23 16:40:41 +0200

    Removed obsolete code

--- fclip.cpp
+++ fclip.cpp
@@ -4,8 +4,6 @@ int copy(int argc, TCHAR* argv[]);
 void paste();
 void pasteByHdrop();
 void pasteByFileContents(CLIPFORMAT clFileDescriptor, CLIPFORMAT clFileContents);
-void olePaste();
-void olePaste2();
 
 // usage: fclip [-v|[file1 [file2 [...]]]]
 int run(int argc, TCHAR* argv[])
@@ -15,9 +13,6 @@ int run(int argc, TCHAR* argv[])
 		paste();
 	else
 		copy(argc, argv);
-
-	std::string wait;
-	std::getline(std::cin, wait);
 
     return 0;
 }
@@ -80,19 +75,8 @@ void paste()
 		pasteByFileContents(formatFileDescriptor, formatFileContents);
 		return;
 	}
-	wprintf(L"Nothing to paste!\n");
+	_tprintf(_T("Nothing to paste!\n"));
 }
-
-/**
- * OleGetClipboard() => IDataObject
- * IDataObject::EnumFormatEtc => IEnumFORMATETC
- * IEnumFORMATETC::next() iterate and search for appropriate IDs
- * 1. Call RegisterClipboardFormat() with
- *   - CFSTR_FILEDESCRIPTOR
- *   - CFSTR_FILECONTENTS
- * 2. Call pDataObject->GetData() with received ID from 1.
- * 3. This creates STGMEDIUM structures which might provide IStream
- */
 
 void pasteByHdrop()
 {
@@ -125,73 +109,11 @@ void pasteByHdrop()
 		_tcscpy(newFiles[i], oldFiles[i]);
 		PathStripPath(newFiles[i]);
 		startFiles += fileNameLength;
-		wprintf(L"copy %ls %ls\n", oldFiles[i], newFiles[i]);
-		//CopyFile(oldFiles[i], newFiles[i], FALSE);
+		_tprintf(_T("copy %ls %ls\n"), oldFiles[i], newFiles[i]);
+		CopyFile(oldFiles[i], newFiles[i], FALSE);
 	}
 	
 	int lala = 0;
-}
-
-void olePaste2()
-{//49366
-	int cntClipFormat = CountClipboardFormats();
-	wprintf(L"Found %i Clipboard Formats.\n", cntClipFormat);
-	UINT iClipFormat = 0;
-	OpenClipboard(NULL);
-	while (iClipFormat = EnumClipboardFormats(iClipFormat))
-	{
-		const int cntMax = 255;
-		TCHAR szFormatName[255];
-		GetClipboardFormatName(iClipFormat, szFormatName, cntMax);
-		wprintf(L"\t %i: %ls\n", iClipFormat, szFormatName);
-	}
-	CloseClipboard();
-
-	UINT iFileContentsFormat = RegisterClipboardFormat(CFSTR_FILECONTENTS);
-	if (!IsClipboardFormatAvailable(iFileContentsFormat))
-	{
-		wprintf(L"CFSTR_FILECONTENTS not available!\n");
-		return;
-	}
-	UINT iFileDescriptorFormat = RegisterClipboardFormat(CFSTR_FILEDESCRIPTOR);
-	if (!IsClipboardFormatAvailable(iFileDescriptorFormat))
-	{
-		wprintf(L"CFSTR_FILEDESCRIPTOR not available!\n");
-		return;
-	}
-	
-	HRESULT result;
-	IDataObject* dataObject;
-	result = OleInitialize(NULL);
-	result = OleGetClipboard(&dataObject);
-
-	FORMATETC fmtFileDescriptor{
-		iFileDescriptorFormat, NULL, DVASPECT_CONTENT, -1, TYMED_HGLOBAL
-	};
-	FORMATETC fmtFileContents{
-		iFileContentsFormat, NULL, DVASPECT_CONTENT, -1, TYMED_ISTREAM
-	};
-	STGMEDIUM stgFileDescriptor{ 0 };
-	STGMEDIUM stgFileContents{ 0 };
-	
-	result = dataObject->GetData(&fmtFileDescriptor, &stgFileDescriptor);
-	
-	IDataObject* dataObject2;
-	result = OleGetClipboard(&dataObject2);
-	result = dataObject2->GetData(&fmtFileContents, &stgFileContents);
-	
-	FILEGROUPDESCRIPTOR* fileGrpDescriptor =
-		(FILEGROUPDESCRIPTOR*)GlobalLock(stgFileDescriptor.hGlobal);
-
-	LARGE_INTEGER pos = { 0, 0 };
-	const int cntBuffer = 1024;
-	BYTE buffer[cntBuffer];
-	ULONG read;
-	result = stgFileContents.pstm->Seek(pos, STREAM_SEEK_SET, NULL);
-	result = stgFileContents.pstm->Read(buffer, cntBuffer, &read);
-
-	GlobalUnlock(stgFileDescriptor.hGlobal);
-	OleUninitialize();
 }
 
 void pasteByFileContents(CLIPFORMAT clFileDescriptor, CLIPFORMAT clFileContents)
@@ -214,7 +136,7 @@ void pasteByFileContents(CLIPFORMAT clFileDescriptor, CLIPFORMAT clFileContents)
 	for (int i = 0; i < pFileGrpDescriptor->cItems; i++)
 	{
 		const FILEDESCRIPTOR& fDescriptor = pFileGrpDescriptor->fgd[i];
-		wprintf(L"File in group descriptor: %ls\n", fDescriptor.cFileName);
+		_tprintf(_T("File in group descriptor: %ls\n"), fDescriptor.cFileName);
 		HANDLE hFile = CreateFile(
 			fDescriptor.cFileName,
 			GENERIC_WRITE,
@@ -252,193 +174,4 @@ void pasteByFileContents(CLIPFORMAT clFileDescriptor, CLIPFORMAT clFileContents)
 		} while (read == ISTREAM_BUF_SIZE);
 		CloseHandle(hFile);
 	}
-}
-
-// http://netez.com/2xExplorer/shellFAQ/adv_clip.html
-void olePaste()
-{
-	HRESULT result;
-	result = OleInitialize(NULL);
-	wprintf(L"\nFormats needed:\n");
-	UINT ufileDesc = RegisterClipboardFormat(
-		CFSTR_FILEDESCRIPTOR
-	);
-	UINT ufileContent = RegisterClipboardFormat(
-		CFSTR_FILECONTENTS
-	);
-	wprintf(L"%i - %ls\n", ufileDesc, CFSTR_FILEDESCRIPTOR);
-	wprintf(L"%i - %ls\n", ufileContent, CFSTR_FILECONTENTS);
-
-	IDataObject* dataObject;
-	result = OleGetClipboard(&dataObject);
-	IEnumFORMATETC* enumFormats;
-	result = dataObject->EnumFormatEtc(DATADIR::DATADIR_GET, &enumFormats);
-	FORMATETC format;
-	wprintf(L"\n\nFormats on the clipboard:\n");
-	FORMATETC formatDescriptor; ZeroMemory(&formatDescriptor, sizeof(FORMATETC));
-	FORMATETC formatContents; ZeroMemory(&formatContents, sizeof(FORMATETC));
-	while (enumFormats->Next(1, &format, NULL) == S_OK)
-	{
-		DWORD tymed;
-		STGMEDIUM medium;
-		const int cchMaxCount = 255;
-		TCHAR lpszFormatName[cchMaxCount];
-		GetClipboardFormatName(
-			format.cfFormat,
-			lpszFormatName,
-			cchMaxCount
-		);
-		tymed = format.tymed;
-		wprintf(L"Found: %i - %ls\n", format.cfFormat, lpszFormatName);
-		if (TYMED_HGLOBAL & tymed) {
-			wprintf(L"\tTYMED_HGLOBAL\n");
-		}
-		if (TYMED_FILE & tymed) {
-			wprintf(L"\tTYMED_FILE\n");
-		}
-		if (TYMED_ISTREAM & tymed) {
-			wprintf(L"\tTYMED_ISTREAM\n");
-		}
-		if (TYMED_ISTORAGE & tymed) {
-			wprintf(L"\tTYMED_ISTORAGE\n");
-		}
-		if (TYMED_GDI & tymed) {
-			wprintf(L"\tTYMED_GDI\n");
-		}
-		if (TYMED_MFPICT & tymed) {
-			wprintf(L"\tTYMED_MFPICT\n");
-		}
-		if (TYMED_ENHMF & tymed) {
-			wprintf(L"\tTYMED_ENHMF\n");
-		}
-		if (TYMED_NULL & tymed) {
-			wprintf(L"\tTYMED_NULL\n");
-		}
-		result = dataObject->GetData(&format, &medium);
-		tymed = medium.tymed;
-		wprintf(L"Medium result:\n");
-		void* p;
-		LARGE_INTEGER pos = { 0, 0 };
-		BYTE pszBuf[1024];
-		ULONG read;
-		FILEGROUPDESCRIPTOR* fileDesc;
-		switch (result)
-		{
-		case S_OK:
-			wprintf(L"S_OK\n");
-			if (TYMED_HGLOBAL & tymed) {
-				wprintf(L"\tTYMED_HGLOBAL\n");
-				HGLOBAL hGlobal = GlobalLock(medium.hGlobal);
-				//fileDesc = (FILEGROUPDESCRIPTOR*)GlobalLock(medium.hGlobal);
-				if (format.cfFormat == RegisterClipboardFormat(CFSTR_PREFERREDDROPEFFECT))
-				{
-					const DWORD& operation = *(DWORD*)hGlobal;
-					
-					if (DROPEFFECT_NONE & operation)
-						wprintf(L"\tNONE\n");
-						
-					if (DROPEFFECT_COPY & operation)
-						wprintf(L"\tCOPY\n");
-						
-					if ( DROPEFFECT_MOVE & operation)
-						wprintf(L"\tMOVE\n");
-						
-					if ( DROPEFFECT_LINK & operation)
-						wprintf(L"\tLINK\n");
-						
-					if ( DROPEFFECT_SCROLL & operation)
-						wprintf(L"\tSCROLL\n");
-						
-				}
-			}
-			if (TYMED_FILE & tymed) {
-				wprintf(L"\tTYMED_FILE\n");
-			}
-			if (TYMED_ISTREAM & tymed) {
-				wprintf(L"\tTYMED_ISTREAM\n");
-				//result = medium.pstm->Seek(pos, STREAM_SEEK_SET, NULL);
-				//result = medium.pstm->Read(pszBuf, 1024, &read);
-			}
-			if (TYMED_ISTORAGE & tymed) {
-				wprintf(L"\tTYMED_ISTORAGE\n");
-			}
-			if (TYMED_GDI & tymed) {
-				wprintf(L"\tTYMED_GDI\n");
-			}
-			if (TYMED_MFPICT & tymed) {
-				wprintf(L"\tTYMED_MFPICT\n");
-			}
-			if (TYMED_ENHMF & tymed) {
-				wprintf(L"\tTYMED_ENHMF\n");
-			}
-			if (TYMED_NULL & tymed) {
-				wprintf(L"\tTYMED_NULL\n");
-			}
-			break;
-		case DV_E_LINDEX:
-			wprintf(L"The value for lindex is not valid; currently, only -1 is supported.\n");
-			break;
-		case DV_E_FORMATETC:
-			wprintf(L"The value for pformatetcIn is not valid.\n");
-			break;
-		case DV_E_TYMED:
-			wprintf(L"The tymed value is not valid.\n");
-			break;
-		case DV_E_DVASPECT:
-			wprintf(L"The dwAspect value is not valid.\n");
-			break;
-		case OLE_E_NOTRUNNING:
-			wprintf(L"The object application is not running.\n");
-			break;
-		case STG_E_MEDIUMFULL:
-			wprintf(L"An error occurred when allocating the medium.\n");
-			break;
-		case E_UNEXPECTED:
-			wprintf(L"An unexpected error has occurred.\n");
-			break;
-		case E_INVALIDARG:
-			wprintf(L"The dwDirection value is not valid.\n");
-			break;
-		case E_OUTOFMEMORY:
-			wprintf(L"There was insufficient memory available for this operation.\n");
-			break;
-		case CLIPBRD_E_BAD_DATA:
-			wprintf(L"Data on clipboard is invalid.\n");
-			break;
-		default:
-			wprintf(L"Unknown error\n");
-		}
-		wprintf(L"\n###\n");
-	}
-
-	if (!*(int*)&formatDescriptor || !*(int*)&formatContents)
-	{
-		wprintf(L"Needed formats not within the clipboard.");
-		return;
-	}
-//	tagTYMED
-//	STGMEDIUM medium;
-//	format.cfFormat = 49265;
-//	format.dwAspect = DVASPECT_CONTENT;
-//	format.lindex = 0;
-//	format.ptd = NULL;
-//	format.tymed = TYMED_ISTREAM;
-//	result = dataObject->GetData(&format, &medium);
-//
-//	STGMEDIUM mediumDescriptor;
-//	result = dataObject->GetData(&formatDescriptor, &mediumDescriptor);
-//	LPCTSTR ptxt = (LPCTSTR)GlobalLock(mediumDescriptor.hGlobal); // dont' forget GlobalUnlock(stgm.hGlobal);
-//
-//	LARGE_INTEGER pos = { 0, 0 };
-//	ULONG i = 0;
-//	BYTE pszBuf[1024];
-//	int nLength = 1024;
-//	STGMEDIUM mediumContents;
-//	//formatContents.cfFormat = 49265;
-//	//formatContents.dwAspect = DVASPECT_CONTENT;
-//	//formatContents.tymed = TYMED_ISTREAM;
-//	result = dataObject->GetData(&formatContents, &mediumContents);
-//	result = mediumContents.pstm->Seek(pos, STREAM_SEEK_SET, NULL);
-//	result = mediumContents.pstm->Read(pszBuf, nLength, &i);
-	int lala = 0;
 }
