2fd222d9d063ece2a3e826a32eb117e2084f25cc
Nico V. Urbanczyk <nico@urbansoft.eu>
2023-04-14 14:42:26 +0200

    separated declaration from definition LastError

--- src/LastError.h
+++ src/LastError.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "pch.h"
+
 class LastError
 {
 public:
@@ -13,10 +13,7 @@ public:
      * 
      * @return New LastError object
      */
-	static LastError New()
-	{
-		return New(GetLastError());
-	}
+	static LastError New();
     /**
      * @brief Creates a new LastError object.
      * 
@@ -27,10 +24,7 @@ public:
      * @param lastErrorCode On of the [system error codes](https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes)
      * @return New LastError object
      */
-	static LastError New(DWORD lastErrorCode)
-	{
-		return LastError{lastErrorCode};
-	}
+	static LastError New(DWORD lastErrorCode);
 private:
 	/// @brief The system error code perceived during creation.
 	DWORD _lastErrorCode;
@@ -44,10 +38,7 @@ public:
      * by the `GetLastError` Win API function.
      * 
      */
-	LastError() : LastError{GetLastError()}
-	{
-		DBGPRINT(L"Default c'tor.");
-	}
+	LastError();
     /**
      * @brief Construct a new Last Error object
      * 
@@ -57,40 +48,24 @@ public:
      * 
      * @param lastErrorCode On of the [system error codes](https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes)
      */
-	LastError(DWORD lastErrorCode) : _lastErrorCode{lastErrorCode}, _lastErrorMessage{nullptr}
-	{
-		DBGPRINT(L"C'tor(lastErrorCode := %d)", _lastErrorCode);
-		setLastErrorMessage();
-	}
+	LastError(DWORD lastErrorCode);
     /**
      * @brief Destroy the Last Error object
      * 
      */
-	~LastError()
-	{
-		DBGPRINT(L"Destructor");
-		resetLastErrorMessage();
-	}
+	~LastError();
 	/**
 	 * @brief Construct a new LastError object by copying from another
 	 * 
 	 * @param other The object which gets copied.
 	 */
-	LastError(const LastError& other) : LastError{other._lastErrorCode}
-	{
-		DBGPRINT(L"Copy c'tor");
-	}
+	LastError(const LastError& other);
 	/**
 	 * @brief Move C'tor semantics
 	 * 
 	 * @param other The object to move from.
 	 */
-	LastError(LastError&& other) noexcept
-		: _lastErrorCode(std::exchange(other._lastErrorCode, 0)),
-    	  _lastErrorMessage(std::exchange(other._lastErrorMessage, nullptr))
-	{
-		DBGPRINT(L"Move c'tor");
-	}
+	LastError(LastError&& other) noexcept;
 
 	/**
 	 * @brief Copy assignment operator.
@@ -98,30 +73,14 @@ public:
 	 * @param other The object which gets copied.
 	 * @return LastError& The copy.
 	 */
-    LastError& operator=(const LastError& other)
-    {
-		DBGPRINT(L"copy assignment");
-        if (this == &other)
-            return *this;
- 
-        _lastErrorCode = other._lastErrorCode;
-		setLastErrorMessage();
- 
-        return *this;
-    }
+    LastError& operator=(const LastError& other);
 	/**
 	 * @brief Move assignment operator.
 	 * 
 	 * @param other The object to move from.
 	 * @return LastError& The moved object.
 	 */
-	LastError& operator=(LastError&& other) noexcept
-    {
-		DBGPRINT(L"move assignment");
-        std::swap(_lastErrorCode, other._lastErrorCode);
-        std::swap(_lastErrorMessage, other._lastErrorMessage);
-        return *this;
-    }
+	LastError& operator=(LastError&& other) noexcept;
     /**
      * @brief Returns the null-terminated current error message.
      * 
@@ -130,19 +89,13 @@ public:
      * 
      * @return LPCWSTR The const pointer to the current error message.
      */
-	LPCWSTR c_str() const
-	{
-		return _lastErrorMessage;
-	}
+	LPCWSTR c_str() const;
     /**
      * @brief Returns the system error code perceived during creation.
      * 
      * @return DWORD The system [system error code](https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes)
      */
-	DWORD getLastError() const
-	{
-		return _lastErrorCode;
-	}
+	DWORD getLastError() const;
 private:
     /**
      * @brief Sets `_lastErrorMessage` back to its original state (`nullptr`).
@@ -154,13 +107,7 @@ private:
      *       [Rule of five](https://en.cppreference.com/w/cpp/language/rule_of_three#Rule_of_five).
      * 
      */
-	void resetLastErrorMessage()
-	{
-		if (_lastErrorMessage != nullptr) {
-			LocalFree(_lastErrorMessage);
-			_lastErrorMessage = nullptr;
-		}
-	}
+	void resetLastErrorMessage();
     /**
      * @brief Set `_lastErrorMessage` according to the current `_lastErrorCode`.
      * 
@@ -169,21 +116,7 @@ private:
      * in `_lastErrorCode`.
      * 
      */
-	void setLastErrorMessage()
-	{
-		resetLastErrorMessage();
-		DWORD msgLength = FormatMessage(
-			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-			NULL,
-			_lastErrorCode,
-			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-			(LPWSTR)&_lastErrorMessage,
-			0,
-			NULL
-		);
-		// remove cr lf
-		_lastErrorMessage[msgLength - 2] = '\0';
-	}
+	void setLastErrorMessage();
     /**
      * @brief Stream operator to use with (UTF-16) output stream.
      * 
